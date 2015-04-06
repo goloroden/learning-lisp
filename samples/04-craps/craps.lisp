8986feb34761c97114db569527647dee7e532892
Golo Roden <webmaster@goloroden.de>
2015-04-06 19:36:45 +0200

    Further improved functions.

--- samples/04-craps/craps.lisp
+++ samples/04-craps/craps.lisp
@@ -10,21 +10,24 @@
 (defun boxcars-p (throw)
   (equal throw '(6 6)))
 
+(defun throw-value (throw)
+  (apply #'+ throw))
+
 (defun instant-win-p (throw)
-  (let ((sum (apply #'+ throw)))
+  (let ((sum (throw-value throw)))
     (or (equal sum 7)
         (equal sum 11))))
 
 (defun instant-loss-p (throw)
-  (let ((sum (apply #'+ throw)))
+  (let ((sum (throw-value throw)))
     (or (equal sum 2)
         (equal sum 3)
         (equal sum 12))))
 
 (defun say-throw (throw)
-  (or (and (snake-eyes-p throw) 'snake-eyes)
-      (and (boxcars-p throw) 'boxcars)
-      (apply #'+ throw)))
+  (cond ((snake-eyes-p throw) 'snake-eyes)
+        ((boxcars-p throw) 'boxcars)
+        (t (throw-value throw))))
 
 (defun craps ()
   (let ((throw (throw-dice)))
@@ -34,7 +37,7 @@
 
 (defun try-for-point (point)
   (let* ((throw (throw-dice))
-         (sum (apply #'+ throw)))
+         (sum (throw-value throw)))
     (cond ((equal sum point) (list 'throw (car throw) 'and (cadr throw) '-- sum '-- 'you 'win))
           ((equal sum 7) (list 'throw (car throw) 'and (cadr throw) '-- sum '-- 'you 'lose))
           (t (list 'throw (car throw) 'and (cadr throw) '-- sum '-- 'throw 'again)))))
