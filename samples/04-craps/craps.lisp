9c605921640a6efe7afc4c0cc5ad556fca054db0
Golo Roden <webmaster@goloroden.de>
2015-04-06 19:30:52 +0200

    Added crips.

---
+++ samples/04-craps/craps.lisp
@@ -0,0 +1,42 @@
+(defun throw-die ()
+  (1+ (random 6)))
+
+(defun throw-dice ()
+  (list (throw-die) (throw-die)))
+
+(defun snake-eyes-p (throw)
+  (and (equal (car throw) 1)
+       (equal (cadr throw) 1)))
+
+(defun boxcars-p (throw)
+  (and (equal (car throw) 6)
+       (equal (cadr throw) 6)))
+
+(defun instant-win-p (throw)
+  (let ((sum (apply #'+ throw)))
+    (or (equal sum 7)
+        (equal sum 11))))
+
+(defun instant-loss-p (throw)
+  (let ((sum (apply #'+ throw)))
+    (or (equal sum 2)
+        (equal sum 3)
+        (equal sum 12))))
+
+(defun say-throw (throw)
+  (or (and (snake-eyes-p throw) 'snake-eyes)
+      (and (boxcars-p throw) 'boxcars)
+      (apply #'+ throw)))
+
+(defun craps ()
+  (let ((throw (throw-dice)))
+    (cond ((instant-win-p throw) (list 'throw (car throw) 'and (cadr throw) '-- (say-throw throw) '-- 'you 'win))
+          ((instant-loss-p throw) (list 'throw (car throw) 'and (cadr throw) '-- (say-throw throw) '-- 'you 'lose))
+          (t (list 'throw (car throw) 'and (cadr throw) '-- 'your 'point 'is (say-throw throw))))))
+
+(defun try-for-point (point)
+  (let* ((throw (throw-dice))
+         (sum (apply #'+ throw)))
+    (cond ((equal sum point) (list 'throw (car throw) 'and (cadr throw) '-- sum '-- 'you 'win))
+          ((equal sum 7) (list 'throw (car throw) 'and (cadr throw) '-- sum '-- 'you 'lose))
+          (t (list 'throw (car throw) 'and (cadr throw) '-- sum '-- 'throw 'again)))))
